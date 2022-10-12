1380d4475632107ec7ae7383b055fd33950d11c3
Jullie Angeli Morales <jullieangelimorales05@gmail.com>
2022-10-12 20:10:16 +0800

    Initial commit

---
+++ resources/views/table.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Table</title>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">Laravel.</a>
+            </nav>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Location</th>
+                <th scope="col">Website</th>
+                <th scope="col">Picture</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Spiral Manila</td>
+                <td>Sofitel Philippine Plaza Hotel CCP Complex 1300, Roxas Blvd, Pasay, Metro Manila</td>
+                <td>
+                    <a href="https://www.spiralmanila.com/">https://www.spiralmanila.com/</a>
+                </td>
+                <td>
+                    <img src="/img/spiral.jpg" alt="spiral" class="w-50 h-50"> </img>
+                </td> 
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Nando's</td>
+                <td>Australia, Bahrain, Bangladesh, Botswana, Canada, Fiji, India, Ireland, Malaysia, Mauritius, Namibia, New Zealand, Nigeria, Oman, Pakistan, Qatar, Singapore, South Africa, Swaziland, United Arab Emirates, United Kingdom, United States, Zambia and Zimbabwe.</td>
+                <td>
+                    <a href="https://www.nandos.com/">https://www.nandos.com/</a>
+                </td>
+                <td>
+                    <img src="/img/nandos.jpg" alt="nando's" class="w-50 h-50"> </img>
+                </td> 
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Bag of Beans</td>
+                <td>3WWR+RXC, Ground Floor, Robinsons Summit Ridge, Tagaytay - Nasugbu Hwy, Tagaytay, Cavite</td>
+                <td>
+                    <a href="https://www.facebook.com/bobtagaytay/">https://www.facebook.com/bobtagaytay/</a>
+                </td>
+                <td>
+                    <img src="/img/bob.jpg" alt="bag of beans" class="w-50 h-50"> </img>
+                </td> 
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Chili's</td>
+                <td>2nd Level, Phase 2,
+Ayala Malls Manila Bay,
+Diosdado Macapagal Blvd.,
+Entertainment City, Parañaque City</td>
+                <td>
+                    <a href="https://chilisphilippines.com/">https://chilisphilippines.com/</a>
+                </td>
+                <td>
+                    <img src="/img/chilis.jpg" alt="chilis" class="w-50 h-50"> </img>
+                </td> 
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>Sumo Niku</td>
+                <td>UG/F SM City Bacoor, Gen. Emilio Aguinaldo St., Bacoor,, 4102, Cavite</td>
+                <td>
+                    <a href="https://www.facebook.com/sumoniku/">https://www.facebook.com/sumoniku/</a>
+                </td>
+                <td>
+                    <img src="/img/sumoniku.jpg" alt="spiral" class="w-50 h-50"> </img>
+                </td> 
+            </tr>
+            
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
+<tbody>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
